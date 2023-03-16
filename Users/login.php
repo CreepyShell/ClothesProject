06d6cc64ce7bf41dd6735f39d74419aa282823a6
STUDENT Danylo Khokhlov <Danylo.Khokhlov@students.ittralee.ie>
2023-03-16 10:52:31 +0000

    Updated register/login forms

--- Users/login.php
+++ Users/login.php
@@ -1,4 +1,15 @@
-<?php try {
+<?php 
+
+if (isset($_GET['auth'])) {
+    if ($_GET['auth'] == 'Login') {
+        echo '<form action="login.php" method="post">
+        <label for="email">Email:</label><input type="text" id="email" name="email"><br>
+        <label for="password">Password:</label><input type="password" id="password" name="password"><br>
+        <button type="submit">Submit</button>
+    </form>';
+    }
+}
+try {
     $pdo = new PDO('mysql:host=localhost;dbname=clothes; charset=utf8', 'root', '');
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
@@ -9,9 +20,13 @@
     if ($result->rowCount() > 0) {
         $row = $result->fetch();
         if ($row['password'] == $_POST['password']) {
-            echo 'login';
+            header('location:mainpage.php');
+            return;
         }
+        echo 'password incorrect, try again';
+        return;
     }
+    echo 'The user with given email was not found';
 } catch (PDOException $e) {
     $output = 'Unable to connect to the database server: ' . $e->getMessage();
 }
--- Users/register.php
+++ Users/register.php
@@ -1,22 +1,36 @@
-<?php try {
-    $pdo = new PDO('mysql:host=localhost;dbname=clothes; charset=utf8', 'root', '');
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-    $sql = "SELECT * FROM users WHERE email=:cemail";
-    $result = $pdo->prepare($sql);
-    $result->bindValue(':cemail', $_POST['email']);
-    $result->execute();
-    if ($result->rowCount() == 0) {
-        $password = $_POST['password'];
-        $insertSql = "INSERT INTO USERS VALUES(:cemail, :cpassword)";
-        $resultInsert = $pdo->prepare($sql);
-        $resultInsert->bindValue(':cemail', $_POST['email']);
-        $resultInsert->bindValue(':cpassword', $_POST['password']);
-        $resultInsert->execute();
-        if ($resultInsert->rowCount() == 0) {
-            header('mainpage.php');
-        }
-    }
-} catch (PDOException $e) {
-    $output = 'Unable to connect to the database server: ' . $e->getMessage();
+<?php
+include('header.html');
+if ($_GET['auth'] == 'Register') {
+    echo '<form action="register.php" method="post">
+    <label for="email">Email:</label><input type="text" id="email" name="email"><br>
+    <label for="password">Password:</label><input type="password" id="password" name="password"><br>
+    <label for="conf-password">Confirm password:</label><input type="password" id="conf-password"><br>
+    <button type="submit">Submit</button>
+</form>';
 }
+if (isset('submit')) {
+
+    try {
+        $pdo = new PDO('mysql:host=localhost;dbname=clothes; charset=utf8', 'root', '');
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        $sql = "SELECT * FROM users WHERE email=:cemail";
+        $result = $pdo->prepare($sql);
+        $result->bindValue(':cemail', $_POST['email']);
+        $result->execute();
+        if ($result->rowCount() == 0) {
+            $password = $_POST['password'];
+            $insertSql = "INSERT INTO USERS VALUES(:cemail, :cpassword)";
+            $resultInsert = $pdo->prepare($sql);
+            $resultInsert->bindValue(':cemail', $_POST['email']);
+            $resultInsert->bindValue(':cpassword', $_POST['password']);
+            $resultInsert->execute();
+            if ($resultInsert->rowCount() == 0) {
+                header('mainpage.php');
+            }
+        }
+    } catch (PDOException $e) {
+        $output = 'Unable to connect to the database server: ' . $e->getMessage();
+    }
+}
+include('footer.html');
