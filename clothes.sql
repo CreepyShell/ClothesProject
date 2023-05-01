d7544bc7a22cb2d802cc302df8bb57d1c6b39e59
Poseidno228 <danil.t404@gmail.com>
2023-05-01 09:54:31 +0100

    Inserted default values into db

--- clothes.sql
+++ clothes.sql
@@ -16,6 +16,32 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `product_type`
+--
+
+DROP TABLE IF EXISTS `product_type`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `product_type` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `description` varchar(30) DEFAULT NULL,
+  `category` varchar(30) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `category` (`category`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `product_type`
+--
+
+LOCK TABLES `product_type` WRITE;
+/*!40000 ALTER TABLE `product_type` DISABLE KEYS */;
+INSERT INTO `product_type` VALUES (1,'black','trousers');
+/*!40000 ALTER TABLE `product_type` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `product_types`
 --
 
@@ -28,7 +54,7 @@ CREATE TABLE `product_types` (
   `category` varchar(30) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `category` (`category`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=43 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -37,7 +63,7 @@ CREATE TABLE `product_types` (
 
 LOCK TABLES `product_types` WRITE;
 /*!40000 ALTER TABLE `product_types` DISABLE KEYS */;
-INSERT INTO `product_types` VALUES (1,'black','trousers');
+INSERT INTO `product_types` VALUES (33,'Men\'s black trousers','Trousers'),(34,'Women\'s blue denim jeans','Jeans'),(35,'Men\'s brown leather shoes','Shoes'),(36,'Women\'s red wool coat','Coats'),(37,'Men\'s gray wool sweater','Sweaters'),(38,'Women\'s white cotton t-shirt','T-shirts'),(39,'Men\'s black leather jacket','Jackets'),(40,'Women\'s brown leather boots','Boots'),(41,'Men\'s blue cotton shirt','Shirts'),(42,'Women\'s yellow raincoat','Raincoats');
 /*!40000 ALTER TABLE `product_types` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -60,7 +86,7 @@ CREATE TABLE `products` (
   UNIQUE KEY `name` (`name`),
   KEY `TypeId` (`TypeId`),
   CONSTRAINT `products_ibfk_1` FOREIGN KEY (`TypeId`) REFERENCES `product_types` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=73 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -69,7 +95,7 @@ CREATE TABLE `products` (
 
 LOCK TABLES `products` WRITE;
 /*!40000 ALTER TABLE `products` DISABLE KEYS */;
-INSERT INTO `products` VALUES (11,600.00,1,'Shirt','Black and white','1345',0),(12,50.00,1,'Shirts','For kids','../Images/clothes.jpg',0),(14,14.00,1,'Another updated','product','none',0),(17,123.00,1,'lkjfds','wqer','qwer',123),(18,23.00,1,'lkjdsf','erterwt','yujyuj',1234);
+INSERT INTO `products` VALUES (63,19.99,33,'Product 1','This is the description for product 1','https://example.com/product1.jpg',50),(64,12.50,34,'Product 2','This is the description for product 2','https://example.com/product2.jpg',10),(65,9.99,35,'Product 3','This is the description for product 3','https://example.com/product3.jpg',20),(66,8.75,33,'Product 4','This is the description for product 4','https://example.com/product4.jpg',15),(67,6.99,37,'Product 5','This is the description for product 5','https://example.com/product5.jpg',30),(68,14.99,42,'Product 6','This is the description for product 6','https://example.com/product6.jpg',8),(69,29.99,39,'Product 7','This is the description for product 7','https://example.com/product7.jpg',40),(70,18.25,34,'Product 8','This is the description for product 8','https://example.com/product8.jpg',24),(71,11.99,41,'Product 9','This is the description for product 9','https://example.com/product9.jpg',18),(72,5.50,36,'Product 10','This is the description for product 10','https://example.com/product10.jpg',6);
 /*!40000 ALTER TABLE `products` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -91,7 +117,7 @@ CREATE TABLE `purchase_items` (
   KEY `user_id` (`user_id`),
   CONSTRAINT `purchase_items_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `products` (`product_id`),
   CONSTRAINT `purchase_items_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=49 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -146,7 +172,7 @@ CREATE TABLE `users` (
   `password` varchar(20) NOT NULL,
   PRIMARY KEY (`user_id`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -155,7 +181,6 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'danil.t404@gmail.com','12345678');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -168,4 +193,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-04-29 20:50:01
+-- Dump completed on 2023-05-01  9:52:17
